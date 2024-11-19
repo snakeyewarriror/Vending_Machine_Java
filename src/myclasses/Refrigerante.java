8399dd90d707d5bdae24a5e55176b3d606a7b7a8
CmF <73684009+cmf0@users.noreply.github.com>
2024-11-19 15:35:52 +0000

    Update Refrigerante.java
    mete os atributos das classes privados, animal. (encapsulamento)

--- src/myclasses/Refrigerante.java
+++ src/myclasses/Refrigerante.java
@@ -12,8 +12,8 @@ public class Refrigerante extends Product{
     }
     
     // atributes
-    String type; // Com acuçar, Sem acuçar
-    String brand;
+    private String type; // Com acuçar, Sem acuçar
+    private String brand;
     
     // methods
     public String getType() {
